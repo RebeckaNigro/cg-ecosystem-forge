a770ef924d556d132243f7334856e07eab107865
Victor Gimenez <victor.gimenez@sesims.com.br>
2022-03-14 10:28:19 -0400

    Parcial validação fale conosco e útil

--- Ecossistema.API/Ecossistema.API/Services/FaleConoscoService.cs
+++ Ecossistema.API/Ecossistema.API/Services/FaleConoscoService.cs
@@ -1,4 +1,5 @@
 ﻿using Ecossistema.API.Models.DTO;
+using Ecossistema.API.Util;
 
 namespace Ecossistema.API.Services
 {
@@ -7,6 +8,7 @@ namespace Ecossistema.API.Services
         public async Task<string> Registrar(FaleConoscoDTO obj)
         {
             //validações
+            Validar(obj);
 
 
             if (!await GravarMensagem(obj))
@@ -26,7 +28,78 @@ namespace Ecossistema.API.Services
 
         private Task<bool> EnviarEmailFaleConosco(FaleConoscoDTO obj)
         {
+
             return Task.FromResult(false);
         }
+
+        private bool Validar(FaleConoscoDTO obj)
+        {
+            if (obj == null) return false;
+            if(!ValidarNome(obj)) return false;
+            if(!ValidarEmailCorporativo(obj)) return false;
+            if(!ValidarTamanhoEmailCorporativo(obj)) return false;
+            if(!ValidarEmailCorporativoValido(obj)) return false;
+            if(!ValidarNome(obj)) return false;
+            if(!ValidarNome(obj)) return false;
+
+            return true;
+        }
+
+        private bool ValidarNome(FaleConoscoDTO obj)
+        {
+            if (!ValidacaoUtil.ValidarString(obj.Nome))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidarEmailCorporativo(FaleConoscoDTO obj)
+        {
+            if (!ValidacaoUtil.ValidarString(obj.EmailCorporativo))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidarTamanhoEmailCorporativo(FaleConoscoDTO obj)
+        {
+            var tamanhoCampo = 250;
+            if (!ValidacaoUtil.ValidarTamanhoString(obj.EmailCorporativo, tamanhoCampo))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidarEmailCorporativoValido(FaleConoscoDTO obj)
+        {
+            if (!ValidacaoUtil.ValidaEMail(obj.EmailCorporativo))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidarTelefone(FaleConoscoDTO obj)
+        {
+            if (!ValidacaoUtil.ValidarString(obj.Telefone))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidarTamanhoTelefone(FaleConoscoDTO obj)
+        {
+            var tamanhoCampo = 15;
+            if (!ValidacaoUtil.ValidarTamanhoString(obj.Telefone, tamanhoCampo))
+            {
+                return false;
+            }
+            return true;
+        }
+
     }
 }
