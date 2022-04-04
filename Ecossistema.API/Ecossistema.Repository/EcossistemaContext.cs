d1c96f5b80d575034a08fa26693915213f85cbcc
Anderson Nunes Tibana <anderson.tibana@sesims.com.br>
2022-04-04 17:20:01 -0400

    Insercao do controller de autenticacao

--- Ecossistema.API/Ecossistema.Repository/EcossistemaContext.cs
+++ Ecossistema.API/Ecossistema.Repository/EcossistemaContext.cs
@@ -1,4 +1,6 @@
 ﻿using Ecossistema.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
