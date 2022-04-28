216338fd0a8430eee164a1ad71ce1542b370e815
Anderson Tibana <anderson.tibana@sesims.com.br>
2022-04-28 09:30:08 -0400

    Função de redefinir senha quase pronta

---
+++ Ecossistema.API/Ecossistema.Services/Dto/RedefinirSenhaDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecossistema.Services.Dto
+{
+    public class RedefinirSenhaDto
+    {
+        public string Email { get; set; }
+        public string OTP { get; set; }
+        public string NovaSenha { get; set; }
+    }
+}
