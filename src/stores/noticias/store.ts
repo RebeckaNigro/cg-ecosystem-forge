511fc440924b47967c8da1299c09d10d10859888
Juliendy Mendes <estagio.startup08@sesims.com.br>
2022-11-09 09:32:16 -0400

    chore: news form input validation

--- src/stores/noticias/store.ts
+++ src/stores/noticias/store.ts
@@ -1,21 +1,40 @@
 import { defineStore } from "pinia";
 import { Noticia } from "./types";
 import { httpRequest } from "../../utils/http";
+import { validateNoticiaInput } from "../../utils/noticias/validation";
+import { GeneralResponseHandler } from "../../utils/GeneralResponseHandler";
 
 export const useNoticiaStore = defineStore('noticiaStore', {
   state: () => {
     return {
-      novaNoticia: new Noticia()
+      novaNoticia: new Noticia("","","","",""),
+	  novaNoticiaResponse: new GeneralResponseHandler(0, 'none', 'no request made yet')
     }
   },
   persist: true,
   actions: {
     async putNews() {
       try {
-        const response = await httpRequest.post('/api/noticia/incluir', this.novaNoticia)
+		const noticiaInput = validateNoticiaInput({id: "", titulo: this.novaNoticia.titulo, descricao: this.novaNoticia.descricao, subTitulo: this.novaNoticia.subTitulo, dataPublicacao: this.novaNoticia.dataPublicacao})
+
+		if(noticiaInput instanceof Noticia){
+			const response = await httpRequest.post('/api/noticia/incluir', this.novaNoticia)
+			this.novaNoticiaResponse.putResponse(response.data.codigo, response.data.dado, response.data.resposta)
+		}else{
+			this.novaNoticiaResponse.putError(223, noticiaInput.message)
+		}
+        
       } catch (error) {
-        console.error(error)
+		if(error instanceof TypeError){
+			this.novaNoticiaResponse.putError(222, error.message)
+		}else{
+			this.novaNoticiaResponse.putError(661, 'Entre em contato com a Startup do SESI')
+			console.error(error)
+		}
+        
       }
+
+	  return false
     }
   },
   getters: {
---
+++ src/utils/noticias/validation.ts
@@ -0,0 +1,31 @@
+import { INoticia, Noticia } from "../../stores/noticias/types"
+
+const titleIsValid = (t: string) => {
+	if(!t || t.length < 2) return false
+	return true
+}
+
+const subtitleIsValid = (s: string) => {
+	if (!s || s.length < 2) return false
+	return true
+}
+
+const dateIsValid = (date: string) => {
+	if(!date) return false
+	return true
+}
+
+const descriptionIsValid = (d: string) => {
+	if(!d || d.length < 10) return false
+	return true
+}
+
+// TODO validar foto
+
+export const validateNoticiaInput = ({id, titulo, descricao, subTitulo, dataPublicacao}: INoticia) : TypeError | Noticia => {
+	if(!titleIsValid(titulo)) return TypeError("Insira um título.")
+	if(!subtitleIsValid(subTitulo)) return TypeError("Insira um subtítulo.")
+	if(!dateIsValid(dataPublicacao)) return TypeError("Selecione uma data.")
+	if(!descriptionIsValid(descricao)) return TypeError("Insira uma descrição.")
+	return new Noticia(id, titulo, descricao, subTitulo, dataPublicacao)
+}
