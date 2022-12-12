b0cac36516571b4376776bf9acb73d7518a953a3
Juliendy Mendes <estagio.startup08@sesims.com.br>
2022-12-12 13:14:18 -0400

    ui: get event image from server and show image input on event form

--- src/utils/constantes.ts
+++ src/utils/constantes.ts
@@ -4,3 +4,8 @@ import { AxiosInstance } from "axios";
 export const CONSTANTES = {
   defaultUrl: 'https://www.integra.sesims.com.br' // FIXME: when possible, change to ecossistema api url
 }
+
+// origem: 5 -> evento; 4 -> noticia
+export const buildImageSrc = (eventId: number, eventName: string, origem: number): string=> {
+	return `http://dev-api.ecossistemadeinovacaocg.com.br/api/documento/downloadDocumento?id=${eventId}&nome=${eventName}&origem=${origem}`
+}
