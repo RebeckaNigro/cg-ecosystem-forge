b4119d46d39565b03acc0b7e61104cff658b7d2f
Anderson Tibana <anderson.tibana@sesims.com.br>
2023-04-17 16:08:13 -0400

    Inserção do link de download em detalhes do documento

---
+++ Ecossistema.API/Ecossistema.Services/Dto/UrlStringsDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecossistema.Services.Dto
+{
+    public class UrlStringsDto
+    {
+        public string ApiUrl { get; set; }
+    }
+}
