18070da85cc6e0361aed667a9866856441ef900c
Juliendy Mendes <estagio.startup08@sesims.com.br>
2023-06-23 12:51:07 -0400

    ui: visualizacao de evento expandido responsivo e fiel ao prototipo

--- src/views/eventos/expandido/EventoExpandido.vue
+++ src/views/eventos/expandido/EventoExpandido.vue
@@ -12,16 +12,16 @@
           <li class="breadcrumb-item unactive" aria-current="page">
             Destaques
           </li>
-          <li class="breadcrumb-item active" aria-current="page">
+          <li class="breadcrumb-item active fw-bold" aria-current="page">
             {{ encerrado ? evento.titulo + ' (ENCERRADO)' : evento.titulo }}
           </li>
         </ol>
       </nav>
     </div>
-    <section class="evento-expandido ghp box pt-3">
+    <section class="evento-expandido box">
       <div class="mb-5">
         <img
-          class="w-100"
+          class="img-fluid"
           :src="
             evento.arquivos[0]
               ? 'data:image/png;base64, ' + evento.arquivos[0].arquivo
@@ -31,62 +31,66 @@
         />
       </div>
 
-      <h1 class="text-start">{{ encerrado ? evento.titulo + ' (ENCERRADO)' : evento.titulo }}</h1>
-      <div class="row container-fluid">
-        <div class="col-sm-8">
+      <h1 class="text-start font-semibold">{{ encerrado ? evento.titulo + ' (ENCERRADO)' : evento.titulo }}</h1>
+      <div class="row container-fluid g-0">
+        <div class=" font-medium p-0 ">
+			<!--RESPONSAVEL-->
           <div class="d-flex mt-4 mb-2">
             <img
               src="/icons/user-circle.svg"
               alt="icone usuario"
-              class="icone-usuario"
             />
-            <div class="d-flex align-items-center mx-2 fw-bold">
+            <div class="d-flex align-items-center mx-2 ">
               {{ evento.responsavel }}
             </div>
           </div>
 
+		  <!--DATA-->
           <div class="d-flex mt-4 mb-2">
             <img
               src="/icons/calendar-icon.svg"
               alt="icone pino de endereço"
-              class="icone-usuario"
             />
-            <div class="d-flex align-items-center mx-2 fw-bold">
+            <div class="d-flex align-items-center mx-2">
               {{ dataFormatada }}
             </div>
           </div>
 
-          <div class="d-flex mt-4 mb-2">
-            <img
-              src="/icons/pin-icon.svg"
-              alt="icone pino de endereço"
-              class="icone-usuario"
-            />
-            <div class="d-flex align-items-center mx-2 fw-bold">
-              {{
-                evento.tipoEventoId == 1
-                  ? enderecoFormatado
-                  : `Online: ${evento.linkExterno}`
-              }}
-            </div>
-          </div>
-        </div>
-        <div class="d-flex col-sm-4 align-items-end justify-content-end">
-          <button type="button" class="green-btn-primary mx-2">
-            <a :href="generatedLink" class="link-btn">Acessar Link</a>
-          </button>
+		  <div class="row align-items-center">
+			  <!--ENDERECO-->
+			  <div class="d-flex mt-4 mb-2 col-md-8">
+				<img
+				  src="/icons/pin-icon.svg"
+				  alt="icone pino de endereço"
+				/>
+				<div class="d-flex text-start align-items-center mx-2">
+				  {{
+					evento.tipoEventoId == 1
+					  ? enderecoFormatado
+					  : `Online: ${evento.linkExterno}`
+				  }}
+				</div>
+			  </div>
+			  <!--BOTAO-->
+			  <div class="d-flex col-md-4 mt-md-0 mt-4 justify-content-end">
+				<button type="button" class="green-btn-primary mx-2">
+				  <a :href="generatedLink" class="link-btn">Acessar Link</a>
+				</button>
+			  </div>
+		  </div>
         </div>
       </div>
 
       <hr class="w-100 my-4" />
 
+	  <!--DESCRICAO-->
       <div id="corpo" class="content ql-editor" v-html="evento.descricao"></div>
     </section>
 
     <div class="btn-up-container" @click="handleNavigateUp">
       <button class="btn-up">
         <img src="/public/arrow_up.svg" alt="Seta para cima" />
-        Subir ao topo
+        Subir pro topo
       </button>
     </div>
   </div>
@@ -164,6 +168,9 @@
 </script>
 
 <style scoped lang="scss">
+  .evento-expandido{
+	padding: 64px;
+  }
   .link-btn {
     text-decoration: none;
     color: white;
@@ -180,6 +187,7 @@
       border: 1px solid #505050;
       border-radius: 10px;
       font-weight: 600;
+	  width: 200px;
     }
 
     .btn-up:hover {
@@ -187,7 +195,9 @@
     }
   }
 
-  .icone-usuario {
-    width: 30px;
+  @media screen and (max-width: 580px) {
+	.evento-expandido{
+		padding: 24px;
+	}
   }
 </style>
