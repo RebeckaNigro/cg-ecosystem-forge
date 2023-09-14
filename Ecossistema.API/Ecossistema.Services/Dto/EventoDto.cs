f49996ea221fba45547a758631215c58e92aa050
Anderson Tibana <anderson.tibana@sesims.com.br>
2023-09-14 14:15:47 -0400

    AreaEvento pronta

--- Ecossistema.API/Ecossistema.Services/Dto/EventoDto.cs
+++ Ecossistema.API/Ecossistema.Services/Dto/EventoDto.cs
@@ -12,6 +12,7 @@ namespace Ecossistema.Services.Dto
         public int? Id { get; set; }
         public int? InstituicaoId { get; set; }
         public int? TipoEventoId { get; set; }
+        public int? AreaEventoId { get; set; }
         public string? Titulo { get; set; }
         public string? Descricao { get; set; }
         public DateTime? DataInicio { get; set; }
