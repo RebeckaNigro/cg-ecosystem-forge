f82a8788974939e0e12aa40342ddb213667f71d7
Juliendy Mendes <juliendy0708@gmail.com>
2023-09-15 13:04:03 -0400

    feat: alteracoes de layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,23 +13,25 @@
 
 			<div class="meet-eco-container p-5 border-0">
 
-				<h2 class="light-title text-uppercase">Conheça o ecossistema</h2>
+				<h2 class="light-title text-uppercase">Faça parte do ecossistema</h2>
 				<div class="row mt-5">
 
 					<div class="col-md-6 d-flex flex-column justify-content-center ">
-						<div class="d-flex flex-column align-self-center ms-lg-5 ">
+						<div class="d-flex flex-column align-self-center ms-lg-5 info-box">
 
 							<p class="text mb-2">
-								Entenda como surgiu e qual o proprósito da comunidade
+								Nosso propósito é disponibilizar caminhos norteadores para todos que desejam empreender, crescer e inovar, independente do formato ou tamanho do negócio. Quer fazer parte disso? Faça adesão ao Pacto de Inovação abaixo.
 							</p>
 
-							<button type="button" class="white-btn-primary text-uppercase align-self-start mt-4"
-								@click="$router.push('/quem-somos')">Saiba mais</button>
+							
+							<a class="white-btn-primary text-uppercase text-center align-self-center text-decoration-none" href="https://www.change.org/p/ades%C3%A3o-ao-pacto-de-inova%C3%A7%C3%A3o-ecossistema-de-inova%C3%A7%C3%A3o-de-campo-grande?recruiter=912086725&recruited_by_id=9066d550-e374-11e8-ba51-2f1d6e951c87&utm_source=share_petition&utm_campaign=share_for_starters_page&utm_medium=copylink" target="_blank">Aderir ao pacto</a>
+							
 						</div>
 
 					</div>
 
-					<div class="col-md-6 d-none d-md-flex">
+					<div class="col-md-6 d-none d-md-flex position-relative foto">
+						<div class="borda position-absolute"></div>
 						<img src="/home/people_and_postits.png" alt="Logo Ecossistema" class="meet-img align-self-start">
 					</div>
 				</div>
@@ -45,13 +47,13 @@
 						<img src="/home/white_logo.png" alt="Logo Ecossistema" class="eco-logo img-fluid p-5">
 					</div>
 
-					<div class="col-md-6 d-flex flex-column justify-content-center">
+					<div class="col-md-6 col-lg-5 d-flex flex-column justify-content-center">
 						<p class="text mb-2 mt-3 mt-lg-0">
 							A comunidade de Inovação em Campo Grande-MS está reunida para ajudar você a se qualificar,
 							estruturar o seu projeto, abrir o seu negócio, facilitar processos e estimular o desenvolvimento
 							socioeconômico local.
 						</p>
-						<p class="text mt-0">Quer fazer parte? </p>
+						<p class="text mt-3 semibold-text fs-4">Quer fazer parte? </p>
 						<button type="button" class="white-btn-primary text-uppercase mt-4"
 							@click="$router.push('/fale-conosco')">Fale conosco</button>
 					</div>
@@ -122,9 +124,11 @@ const logosParceiros: string[] = [
 </script>
 
 <style scoped lang="scss">
+.semibold-text{
+	font-weight: 600!important;
+}
 section.container-fluid {
 	padding: 0;
-
 
 	.meet-eco-container,
 	.more-info-container {
@@ -133,46 +137,73 @@ section.container-fluid {
 		}
 
 		p.text {
-			font-weight: 600;
+			font-weight: 500;
 			color: #fff;
-			text-align: justify;
+			text-align: start;
 			font-size: 1.2rem;
 		}
 	}
 
 	.meet-eco-container {
-		background: linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, rgba(255, 255, 255, 0.2) 100%), #639280;
+		background: var(--linear-gradient-orange-1);
+		height: 605px;
 
-
-		p.text {
-			max-width: 300px;
-		}
-
-		button {
-			width: 70%;
-			font-size: 1.1rem;
+		.info-box{
+			width: 60%;
+			p.text {
+				max-width: 490px;
+			}
+	
+			a {
+				color: #103265;
+				width: 100%;
+				font-size: 1.1rem;
+				padding-top: 10px;
+				margin-top: 3rem;
+			}
+	
+			a:hover{
+				color: #103265;
+			}
 		}
 
 		.meet-img {
 			border-radius: 10px;
 			width: 70%;
 		}
+
+		.foto{
+			z-index: 10;
+			.borda{
+				top: 7px;
+				left: 10px;
+				display: flex;
+				height: 95%;
+				width: 68%;
+				border-radius: 15px;
+				border: 2px solid #158A39;
+				transform: skew(-12deg, -7deg);
+				z-index: -1;
+				
+			}
+		}
 	}
 
 	.more-info-container {
-		background: linear-gradient(180deg, rgba(255, 255, 255, 0.2) 0%, rgba(0, 0, 0, 0.2) 100%), #639280;
+		background: var(--linear-gradient-orange-2);
 
 		p.text {
 			max-width: 560px;
 		}
 
 		button {
-			width: 35%;
+			width: 90%;
 			font-size: 1.1rem;
+			color: #103265;
 		}
 
 		.eco-logo {
-			width: 50%;
+			width: 60%;
 		}
 	}
 }
@@ -188,8 +219,8 @@ section.container-fluid {
 }
 
 .circle-bottom {
-	top: -0.1rem;
-	background: #81A799;
+	top: -0.01rem;
+	background: #C7845C;
 
 }
 
@@ -207,8 +238,26 @@ section.container-fluid {
 
 
 		.meet-eco-container {
-			p.text {
-				max-width: 450px;
+			height: 900px;
+			.info-box{
+				p.text {
+					max-width: 700px;
+				}
+	
+				a{
+					font-size: 1.5rem;
+				}
+
+			}
+		}
+
+		.more-info-container{
+			p.text{
+				max-width: 700px;
+			}
+
+			p.semibold-text{
+				font-size: 2rem!important;
 			}
 		}
 
@@ -244,14 +293,39 @@ section.container-fluid {
 	section.container-fluid {
 		padding: 0;
 
+		.meet-eco-container{
+			height: 550px;
+			.info-box{
+				width: 80%;
+			}
+			.foto{
+				.meet-img{
+					width: 100%;
+	
+				}
+				.borda{
+					top: 5px;
+					left: 10px;
+					display: flex;
+					height: 60%;
+					width: 90%;
+					border-radius: 15px;
+					border: 2px solid #158A39;
+					transform: skew(-15deg, -7deg);
+					z-index: -1;
+				}
+			}
+		}
+
 		.more-info-container,
 		.meet-eco-container {
 			p.text {
-				font-size: .9rem;
+				font-size: 1rem;
+				max-width: 700px;
 			}
 
-			button {
-				width: 60%;
+			a, button {
+				width: 100%;
 				font-size: 1rem;
 			}
 
@@ -269,27 +343,45 @@ section.container-fluid {
 @media(max-width: 580px) {
 	section.container-fluid {
 		padding: 0;
-
-
 		.meet-eco-container {
-			height: 380px;
+			height: 600px;
+			h2.light-title{
+				font-size: 1.6rem;
+			}
+			.info-box{
+				width: 100%;
+				p.text{
+					max-width: 500px;
+					font-size: 1.1rem;
+			
+		
+				}
+				a.white-btn-primary{
+					width: 100%;
+					margin: 1.4rem auto;
+					font-size: 1.1rem;
+					padding-top: 12px;
+				}
+
+			}
 		}
+
 
 		.more-info-container {
-			height: 450px;
-		}
-
-		.more-info-container,
-		.meet-eco-container {
-
+			height: 600px;
+			padding: 2rem!important;
 			h2.light-title {
-				font-size: 1.5rem;
+				font-size: 1.6rem;
+			}
+
+			p.text{
+				font-size: 1.1rem;
 			}
 
 			button {
-				width: 60%;
+				width: 100%;
 				margin: .7rem auto;
-				font-size: .9rem;
+				font-size: 1.2rem;
 			}
 		}
 	}
