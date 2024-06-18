c1cef98f3395bdf2a6a0b2195b913b66bb1d37c1
Leonardo Kazuyoshi Takahashi da Silva <lkazu@duck.com>
2024-06-18 17:40:55 -0400

    work?

--- src/stores/eventos/store.ts
+++ src/stores/eventos/store.ts
@@ -1,17 +1,24 @@
 //@ts-nocheck
-import { IEvento, EnderecoExistente, IUltimoEvento, Evento, IEventoSimplificado, IOrganizador } from "./types"
-import { defineStore } from "pinia"
-import { httpRequest, getLastContent } from "../../utils/http"
-import { validateEventoInput } from "../../utils/eventos/validation"
-import { GeneralResponseHandler } from "../../utils/GeneralResponseHandler"
+import {
+  IEvento,
+  EnderecoExistente,
+  IUltimoEvento,
+  Evento,
+  IEventoSimplificado,
+  IOrganizador,
+} from "./types";
+import { defineStore } from "pinia";
+import { httpRequest, getLastContent } from "../../utils/http";
+import { validateEventoInput } from "../../utils/eventos/validation";
+import { GeneralResponseHandler } from "../../utils/GeneralResponseHandler";
 
-const enderecosExistentes: Array<EnderecoExistente> = []
-const ultimosEventos: Array<IEventoSimplificado> = []
-const eventos: Array<IUltimoEvento> = []
-const eventosUsuarioLogado: Array<IEventoSimplificado> = []
-const ultimosEventosUsuarioLogado: Array<IUltimoEvento> = []
-const organizadores: IOrganizador[] = []
-const eventosFiltrados: Array<IEventoSimplificado> = []
+const enderecosExistentes: Array<EnderecoExistente> = [];
+const ultimosEventos: Array<IEventoSimplificado> = [];
+const eventos: Array<IUltimoEvento> = [];
+const eventosUsuarioLogado: Array<IEventoSimplificado> = [];
+const ultimosEventosUsuarioLogado: Array<IUltimoEvento> = [];
+const organizadores: IOrganizador[] = [];
+const eventosFiltrados: Array<IEventoSimplificado> = [];
 
 export const useEventoStore = defineStore("eventoStore", {
   state: () => {
@@ -21,69 +28,77 @@ export const useEventoStore = defineStore("eventoStore", {
       response: new GeneralResponseHandler(0, "none", "no request made yet"),
       eventos,
       eventosUsuarioLogado,
-	  ultimosEventosUsuarioLogado,
+      ultimosEventosUsuarioLogado,
       loadRascunho: false,
-	  organizadores,
-	  eventosFiltrados,
-	  filterPage: 1
-    }
+      organizadores,
+      eventosFiltrados,
+      filterPage: 1,
+    };
   },
   persist: false,
   actions: {
     async postEvent(novoEvento: IEvento) {
-		console.log(novoEvento);
-		
+      const data = {
+        ...novoEvento,
+        endereco: {
+          ...novoEvento.endereco,
+          cep: novoEvento.endereco.cep.split("-").join(""),
+        },
+      };
+
       try {
-        const formData = new FormData()
-        formData.append("evento", JSON.stringify(novoEvento))
+        const formData = new FormData();
+        formData.append("evento", JSON.stringify(data));
 
         novoEvento.tags.forEach((tag, index) => {
-          formData.append(`tags[${index}].descricao`, tag.descricao)
-        })
+          formData.append(`tags[${index}].descricao`, tag.descricao);
+        });
 
-        formData.append("arquivo", novoEvento.arquivo)
+        formData.append("arquivo", novoEvento.arquivo);
 
         const response = await httpRequest.post(
           "/api/evento/incluir",
           formData,
           {
-            headers: { "Content-Type": "multipart/form-data" }
+            headers: { "Content-Type": "multipart/form-data" },
           }
-        )
+        );
 
         this.response.putResponse(
           response.data.codigo,
           response.data.dado,
           response.data.resposta
-        )
+        );
       } catch (error) {
         if (error instanceof TypeError) {
-          this.response.putError(222, error.message)
+          this.response.putError(222, error.message);
         } else {
-          this.response.putError(661, "Entre em contato com a Startup do SESI")
-          console.error(error)
+          this.response.putError(661, "Entre em contato com a Startup do SESI");
+          console.error(error);
         }
       }
 
-      return false
+      return false;
     },
 
     async getUserEvents(page: number) {
       try {
-        const response = await httpRequest.get(`/api/evento/listarPorUsuarioId?paginacao=${page}`)
+        const response = await httpRequest.get(
+          `/api/evento/listarPorUsuarioId?paginacao=${page}`
+        );
         if (response.data.codigo === 200) {
           this.response.putResponse(
             response.data.codigo,
             response.data.retorno,
             response.data.resposta
-          )
-		  this.eventosUsuarioLogado = []
+          );
+          this.eventosUsuarioLogado = [];
           for (const event of response.data.retorno) {
-            this.eventosUsuarioLogado.push(event)
+            this.eventosUsuarioLogado.push(event);
           }
         }
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     },
 
@@ -91,66 +106,70 @@ export const useEventoStore = defineStore("eventoStore", {
       try {
         const response = await httpRequest.get(
           `/api/evento/listarEnderecos?tipoEnderecoId=${tipoEvendoId}`
-        )
+        );
         if (response) {
           if (response.data.codigo === 200) {
             const extractExistingAddress = (data: any) => {
-              const address: EnderecoExistente = { ...data }
-              return address
-            }
-            this.enderecosExistentes = []
+              const address: EnderecoExistente = { ...data };
+              return address;
+            };
+            this.enderecosExistentes = [];
             for (const addrr of response.data.retorno) {
-              this.enderecosExistentes.push(extractExistingAddress(addrr))
+              this.enderecosExistentes.push(extractExistingAddress(addrr));
             }
-          } else if (response.data.codigo === 404) this.enderecosExistentes = []
+          } else if (response.data.codigo === 404)
+            this.enderecosExistentes = [];
         }
       } catch (error) {}
     },
 
     async getLastEvents() {
-      const response = await getLastContent("evento")
+      const response = await getLastContent("evento");
       if (response.data.codigo === 200) {
-        this.ultimosEventos = []
+        this.ultimosEventos = [];
         for (const lastEvent of response.data.retorno) {
-          this.ultimosEventos.push(lastEvent)
+          this.ultimosEventos.push(lastEvent);
         }
       }
     },
 
-	async getUserLastEvents() {
-		try {
-		  const response = await httpRequest.get("/api/evento/listarUltimosPorUsuarioId")
-		  if (response.data.codigo === 200) {
-			this.response.putResponse(
-			  response.data.codigo,
-			  response.data.retorno,
-			  response.data.resposta
-			)
-			this.ultimosEventosUsuarioLogado = []
-			for (const event of response.data.retorno) {
-			  this.ultimosEventosUsuarioLogado.push(event)
-			}
-		  }else{
-			this.response.putError(response.data.codigo, response.data.resposta)
-		  }
-		} catch (error) {
-		  console.error(error)
-		}
-	  },
+    async getUserLastEvents() {
+      try {
+        const response = await httpRequest.get(
+          "/api/evento/listarUltimosPorUsuarioId"
+        );
+        if (response.data.codigo === 200) {
+          this.response.putResponse(
+            response.data.codigo,
+            response.data.retorno,
+            response.data.resposta
+          );
+          this.ultimosEventosUsuarioLogado = [];
+          for (const event of response.data.retorno) {
+            this.ultimosEventosUsuarioLogado.push(event);
+          }
+        } else {
+          this.response.putError(response.data.codigo, response.data.resposta);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
 
     async getAllEvents(page: number) {
       try {
-        const response = await httpRequest.get(`/api/evento/listarTodos?paginacao=${page.toString()}`)
-        if(response.data.codigo === 200) {
-			if(page === 1){
-				this.eventos = response.data.retorno
-			}else{
-				this.eventos = this.eventos.concat(response.data.retorno)
-			}
-
-		  }
+        const response = await httpRequest.get(
+          `/api/evento/listarTodos?paginacao=${page.toString()}`
+        );
+        if (response.data.codigo === 200) {
+          if (page === 1) {
+            this.eventos = response.data.retorno;
+          } else {
+            this.eventos = this.eventos.concat(response.data.retorno);
+          }
+        }
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     },
 
@@ -158,51 +177,65 @@ export const useEventoStore = defineStore("eventoStore", {
       try {
         const response = await httpRequest.delete(
           `/api/evento/excluir?id=${eventId}`
-        )
+        );
         if (response.data.codigo === 200) {
           this.response.putResponse(
             response.data.codigo,
             response.data.dado,
             response.data.resposta
-          )
+          );
         }
       } catch (error) {
         if (error instanceof TypeError) {
-          this.response.putError(222, error.message)
+          this.response.putError(222, error.message);
         } else {
-          this.response.putError(661, "Erro ao remover evento.")
-          console.error(error)
+          this.response.putError(661, "Erro ao remover evento.");
+          console.error(error);
         }
       }
     },
 
     async putEvent(eventoEdicao: IEvento) {
       try {
-        const formData = new FormData()
-        formData.append("evento", JSON.stringify(eventoEdicao))
+        let data;
+        if (eventoEdicao.endereco) {
+          data = {
+            ...eventoEdicao,
+            endereco: {
+              ...eventoEdicao.endereco,
+              cep: (eventoEdicao.endereco.cep = eventoEdicao.endereco?.cep
+                .split("-")
+                .join("")),
+            },
+          };
+        } else {
+          data = { ...eventoEdicao };
+        }
+        const formData = new FormData();
+        formData.append("evento", JSON.stringify(data));
 
         if (eventoEdicao.tags.length > 0) {
           eventoEdicao.tags.forEach((tag, index) => {
-            formData.append(`tags[${index}].descricao`, tag.descricao)
-          })
+            formData.append(`tags[${index}].descricao`, tag.descricao);
+          });
         }
 
-        formData.append("arquivo", eventoEdicao.arquivos[0].arquivo)
+        formData.append("arquivo", eventoEdicao.arquivos[0].arquivo);
 
         const res = await httpRequest.put("/api/evento/editar", formData, {
-          headers: { "Content-Type": "multipart/form-data" }
-        })
+          headers: { "Content-Type": "multipart/form-data" },
+        });
         this.response.putResponse(
           res.data.codigo,
           res.data.dado,
           res.data.resposta
-        )
+        );
       } catch (error) {
         if (error instanceof TypeError) {
-          this.response.putError(222, error.message)
+          this.response.putError(222, error.message);
         } else {
-          this.response.putError(661, "Entre em contato com a Startup do SESI")
-          console.error(error)
+          this.response.putError(661, "Entre em contato com a Startup do SESI");
+          console.error(error);
         }
       }
     },
@@ -211,46 +244,48 @@ export const useEventoStore = defineStore("eventoStore", {
       try {
         const response = await httpRequest.get(
           `/api/evento/detalhes?Id=${eventoId}`
-        )
+        );
         if (response.data.codigo === 200) {
           this.response.putResponse(
             response.data.codigo,
             response.data.retorno,
             response.data.resposta
-          )
+          );
         }
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     },
-	async getOrganizersList(){
-		try {
-			const response = await httpRequest.get('api/evento/listarOrganizadores')
-	
-			if (response.data.codigo === 200) { 
-				this.organizadores = []
-			  response.data.retorno.forEach((organizador: IOrganizador) => {
-				this.organizadores.push(organizador)
-			  });		  
-			}
-		  } catch (error) {
-			console.error(error)
-		  }
-	},
-	async filterEvents(page: number,organizador: string){
+    async getOrganizersList() {
+      try {
+        const response = await httpRequest.get(
+          "api/evento/listarOrganizadores"
+        );
 
-		try{
-			const response = await httpRequest.get(`/api/evento/listarTodos?paginacao=${page}&organizador=${organizador}`)
-			if (response.data.codigo === 200) {
-				this.eventosFiltrados = []
-				response.data.retorno.forEach((evento: IEventoSimplificado) => {
-				  this.eventosFiltrados.push(evento)
-				});	
-			
-			}
-		} catch (error) {
-			console.error(error)
-		}
-	}
-  }
-})
+        if (response.data.codigo === 200) {
+          this.organizadores = [];
+          response.data.retorno.forEach((organizador: IOrganizador) => {
+            this.organizadores.push(organizador);
+          });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async filterEvents(page: number, organizador: string) {
+      try {
+        const response = await httpRequest.get(
+          `/api/evento/listarTodos?paginacao=${page}&organizador=${organizador}`
+        );
+        if (response.data.codigo === 200) {
+          this.eventosFiltrados = [];
+          response.data.retorno.forEach((evento: IEventoSimplificado) => {
+            this.eventosFiltrados.push(evento);
+          });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+});
